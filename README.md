69ca564c852363fc10f6a952fd96416eb2b5ffe6
Sanae6 <atmatm6000@outlook.com>
2018-09-23 22:09:23 -0600

    my daddy says i need to sleep now :joy: i sleep when i'm tired
    jk ily dad

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# life-server
+# Sanae's Life Calendar
 A calendar server that will support automatic setting of statuses or posting of activities.
+
+The basic use for this is just to put your schedule in a Heroku buildpack, and people will be able to subscribe to your daily schedule via email or other methods of automation
--- index.js
+++ index.js
@@ -9,4 +9,16 @@ var app = express();
 var server = http.createServer(app);
 var wss = new ws.Server({server});
 app.use(express.static(path.join(__dirname,"static")));
-wss.on
+wss.on("connection",(ws)=>{//ws from computer or android phone
+	ws.on("ping",function(){
+		ws.emit("pong");
+	});
+	ws.on("message",(data)=>{
+		var msg = JSON.parse(data);
+		switch (msg.op){
+			case "user":
+				
+		}
+	})
+});
+server.listen(80);
